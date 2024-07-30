76037c369fbcd2b5c65cdad8c3b850e6f148dc14
Kaique Amaral <kaiqueamaral01@gmail.com>
2024-07-30 18:25:33 -0300

    Adicionado classes para lidar com o resposta da API

---
+++ PokeTamagochi/PokeTamagochi/Pokemon.cs
@@ -0,0 +1,15 @@
+﻿namespace PokeTamagochi;
+
+internal class Pokemon
+{
+
+    public Pokemon(string name, string url)
+    {
+        Name = name;
+        Url = url;
+    }
+
+    public string Name { get; }
+    public string Url { get; }
+
+}
---
+++ PokeTamagochi/PokeTamagochi/PokemonApiResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace PokeTamagochi;
+
+internal class PokemonApiResponse
+{
+    public PokemonApiResponse()
+    {
+        Results = new List<Pokemon>();
+    }
+    public int Count { get; set; }
+    public string Next { get; set; }
+    public string Previous { get; set; }
+    public List<Pokemon> Results { get; set; }
+}
