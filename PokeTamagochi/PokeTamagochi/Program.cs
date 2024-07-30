77d04ace9c6b7d78c5ef6e17df8d1d861cd1ed87
Kaique Amaral <kaiqueamaral01@gmail.com>
2024-07-30 18:28:05 -0300

    Adicionado listagem de nomes de pokemons disponíveis

--- PokeTamagochi/PokeTamagochi/Program.cs
+++ PokeTamagochi/PokeTamagochi/Program.cs
@@ -1,21 +1,21 @@
 ﻿using PokeTamagochi;
 using RestSharp;
 using System.Net;
+using Newtonsoft.Json;
 
-Dictionary<string,Pokemon> pokemonsDisponiveis = new();
+PokemonApiResponse pokemonApiResponse;
 
+void InitialScreen()
+{
+    Console.WriteLine("******************************");
+    Console.WriteLine("Bem vindos ao PokeTamagochi!");
+    Console.WriteLine("******************************\n");
 
-//void InitialScreen()
-//{
-//    Console.WriteLine("******************************");
-//    Console.WriteLine("Bem vindos ao PokeTamagochi!");
-//    Console.WriteLine("******************************\n");
+    Console.WriteLine("Aqui estão todas as opções de pokémons disponíveis para serem selecionados:\n");
 
-//    Console.WriteLine("Aqui estão todas as opções de pokémons disponíveis para serem selecionados:\n");
+    DisplayAllAvailablePokemons();
 
-
-
-//}
+}
 
 
 
@@ -23,14 +23,15 @@ void StartConnectionWithAPI()
 {
     try
     {
-        var client = new RestClient("https://pokeapi.co/api/v2/pokmon");
+        var client = new RestClient("https://pokeapi.co/api/v2/pokemon");
         var request = new RestRequest("", Method.Get);
         var response = client.Get(request);
 
-        if (response.StatusCode == HttpStatusCode.OK)
+        if (response.StatusCode == HttpStatusCode.OK && response.Content != null)
         {
-            Console.WriteLine("Conexão estabelicida. Resultado:\n");
-            Console.WriteLine(response.Content);
+            ParseApiResponse(response.Content);
+            InitialScreen();
+
         }
         else
         {
@@ -45,6 +46,22 @@ void StartConnectionWithAPI()
 
 }
 
+void ParseApiResponse(string responseAPI)
+{
+
+    pokemonApiResponse = JsonConvert.DeserializeObject<PokemonApiResponse>(responseAPI);
+
+   
+   
+}
+
+void DisplayAllAvailablePokemons()
+{
+    foreach (Pokemon pokemon in pokemonApiResponse.Results)
+    {
+        Console.WriteLine("Nome: " + pokemon.Name);
+    }
+}
+
 
 StartConnectionWithAPI();
-//InitialScreen(); 
